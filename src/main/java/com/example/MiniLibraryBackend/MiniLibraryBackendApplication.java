101a01aa820cf11974447a944f966bc0ea441923
vaibhav sathe <vaibhavsathe160@gmail.com>
2026-01-11 19:36:00 +0530

    first commit

---
+++ src/main/java/com/example/MiniLibraryBackend/MiniLibraryBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.MiniLibraryBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniLibraryBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniLibraryBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/MiniLibraryBackend/MiniLibraryBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MiniLibraryBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniLibraryBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
