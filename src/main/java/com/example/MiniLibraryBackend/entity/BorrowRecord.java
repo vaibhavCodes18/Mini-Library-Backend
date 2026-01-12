92e70f0cf361a40fded37c49a4d6e79501885678
vaibhav sathe <vaibhavsathe160@gmail.com>
2026-01-12 18:34:02 +0530

    Entities creatation almost done

--- src/main/java/com/example/MiniLibraryBackend/entity/BorrowRecord.java
+++ src/main/java/com/example/MiniLibraryBackend/entity/BorrowRecord.java
@@ -2,6 +2,8 @@ package com.example.MiniLibraryBackend.entity;
 
 import jakarta.persistence.*;
 
+import java.time.LocalDate;
+
 @Entity
 @Table(name = "borrow_records")
 public class BorrowRecord {
@@ -17,5 +19,35 @@ public class BorrowRecord {
     @JoinColumn(name = "book_id", nullable = false)
     private Book book;
 
+    private LocalDate borrowedBook;
+
+    public BorrowRecord() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Member getMember() {
+        return member;
+    }
+
+    public void setMember(Member member) {
+        this.member = member;
+    }
+
+    public Book getBook() {
+        return book;
+    }
+
+    public void setBook(Book book) {
+        this.book = book;
+    }
+
+    
 
 }
