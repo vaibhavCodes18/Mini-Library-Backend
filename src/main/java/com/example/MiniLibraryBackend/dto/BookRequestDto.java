9fdc35bd36cb59456b86f365470ea886f4e12cbf
vaibhav sathe <vaibhavsathe160@gmail.com>
2026-01-13 20:01:10 +0530

    Dto Service added

---
+++ src/main/java/com/example/MiniLibraryBackend/dto/BookRequestDto.java
@@ -0,0 +1,4 @@
+package com.example.MiniLibraryBackend.dto;
+
+public class BookRequestDto {
+}
---
+++ src/main/java/com/example/MiniLibraryBackend/service/BookBorrowServiceImpl.java
@@ -0,0 +1,4 @@
+package com.example.MiniLibraryBackend.service;
+
+public class BookBorrowServiceImpl implements BookBorrowService{
+}
