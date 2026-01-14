5d29f984158be7293d9564def246e8bce8a3bdf0
vaibhav sathe <vaibhavsathe160@gmail.com>
2026-01-14 19:04:33 +0530

    Member add functionality begining

--- src/main/java/com/example/MiniLibraryBackend/dto/MemberRequestDto.java
+++ src/main/java/com/example/MiniLibraryBackend/dto/MemberRequestDto.java
@@ -1,4 +1,29 @@
 package com.example.MiniLibraryBackend.dto;
 
+import jakarta.validation.constraints.NotBlank;
+
 public class MemberRequestDto {
+    @NotBlank(message = "Name is required")
+    private String name;
+    @NotBlank(message = "Email is required")
+    private String email;
+
+    public MemberRequestDto() {
+    }
+
+    public @NotBlank(message = "Name is required") String getName() {
+        return name;
+    }
+
+    public void setName(@NotBlank(message = "Name is required") String name) {
+        this.name = name;
+    }
+
+    public @NotBlank(message = "Email is required") String getEmail() {
+        return email;
+    }
+
+    public void setEmail(@NotBlank(message = "Email is required") String email) {
+        this.email = email;
+    }
 }
