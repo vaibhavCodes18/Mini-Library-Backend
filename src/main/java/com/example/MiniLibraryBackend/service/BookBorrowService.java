fdb76e9b8b98c01da0e8e3a676d05599efba956e
vaibhav sathe <vaibhavsathe160@gmail.com>
2026-01-15 20:20:30 +0530

    We're in the last phase of mini library management system i.e., get borrow record by member

--- src/main/java/com/example/MiniLibraryBackend/service/BookBorrowService.java
+++ src/main/java/com/example/MiniLibraryBackend/service/BookBorrowService.java
@@ -2,7 +2,9 @@ package com.example.MiniLibraryBackend.service;
 
 import com.example.MiniLibraryBackend.dto.BorrowBookRequestDto;
 import com.example.MiniLibraryBackend.dto.BorrowBookResponseDto;
+import com.example.MiniLibraryBackend.dto.BorrowHistoryResponseDto;
 
 public interface BookBorrowService {
     BorrowBookResponseDto borrowBook(BorrowBookRequestDto borrowBookRequestDto);
+    BorrowHistoryResponseDto getBorrowHistoryById(Long borrowId);
 }
